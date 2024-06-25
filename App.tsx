04603493f223ca477c3af7b6e5f82e06c774ad0d
Roman <>
2024-06-25 15:28:59 +0300

    Score

--- App.tsx
+++ App.tsx
@@ -1,25 +1,81 @@
-import { Canvas, Group, Image, useImage } from "@shopify/react-native-skia";
-import React, { useEffect } from "react";
-import { useWindowDimensions } from "react-native";
-import { GestureHandlerRootView, GestureDetector, Gesture } from "react-native-gesture-handler";
-import { Easing, Extrapolation, interpolate, useDerivedValue, useFrameCallback, useSharedValue, withRepeat, withSequence, withTiming } from "react-native-reanimated";
+import { Canvas, Group, Image, Text, matchFont, useFont, useImage } from "@shopify/react-native-skia";
+import React, { useEffect, useState } from "react";
+import { Platform, useWindowDimensions } from "react-native";
+import { Gesture, GestureDetector, GestureHandlerRootView } from "react-native-gesture-handler";
+import {
+  Easing,
+  Extrapolation,
+  interpolate,
+  runOnJS,
+  useAnimatedReaction,
+  useDerivedValue,
+  useFrameCallback,
+  useSharedValue,
+  withRepeat,
+  withSequence,
+  withTiming,
+} from "react-native-reanimated";
 
 const GRAVITY = 1000;
 const JUMP_FORCE = -500;
 
 const App = () => {
   const { width, height } = useWindowDimensions();
-  const r = width * 0.33;
+  const [score, setScore] = useState(0);
+
   const bg = useImage(require('./assets/sprites/background-day.png'));
   const bird = useImage(require('./assets/sprites/yellowbird-upflap.png'));
   const pipeBottom = useImage(require('./assets/sprites/pipe-green.png'));
   const pipeTop = useImage(require('./assets/sprites/pipe-green-top.png'));
   const base = useImage(require('./assets/sprites/base.png'));
+  // const font = useFont(require('./assets/fonts/Roboto-Bold.ttf'), 20);
 
   const x = useSharedValue(width);
 
   const birdY = useSharedValue(height / 3);
+  const birdPosition = {
+    x: width / 4,
+  };
   const birdYVelocity = useSharedValue(0);
+
+  useEffect(() => {
+    x.value = withRepeat(
+      withSequence(
+        withTiming(-150, { duration: 3000, easing: Easing.linear }),
+        withTiming(width, { duration: 0 }),
+      ),
+      -1
+    );
+  }, []);
+
+  useAnimatedReaction(
+    () => x.value,
+    (currentValue, previousValue) => {
+      const middle = birdPosition.x;
+
+      if (
+        currentValue !== previousValue &&
+        currentValue < middle &&
+        previousValue > middle
+      ) {
+        // do something ✨
+        runOnJS(setScore)(score + 1);
+      }
+    }
+  );
+
+  useFrameCallback(({ timeSincePreviousFrame: dt }) => {
+    if (!dt) {
+      return;
+    }
+    birdY.value = birdY.value + (birdYVelocity.value * dt) / 1000;
+    birdYVelocity.value = birdYVelocity.value + (GRAVITY * dt) / 1000;
+  });
+
+  const gesture = Gesture.Tap().onStart(() => {
+    birdYVelocity.value = JUMP_FORCE;
+  });
+
   const birdTransform = useDerivedValue(() => {
     return [{
       rotate: interpolate(
@@ -34,27 +90,13 @@ const App = () => {
     return { x: width / 4 + 32, y: birdY.value + 24 };
   });
 
-  useFrameCallback(({ timeSincePreviousFrame: dt }) => {
-    if (!dt) {
-      return;
-    }
-    birdY.value = birdY.value + (birdYVelocity.value * dt) / 1000;
-    birdYVelocity.value = birdYVelocity.value + (GRAVITY * dt) / 1000;
-  });
-
-  useEffect(() => {
-    x.value = withRepeat(
-      withSequence(
-        withTiming(-150, { duration: 3000, easing: Easing.linear }),
-        withTiming(width, { duration: 0 }),
-      ),
-      -1
-    );
-  }, []);
-
-  const gesture = Gesture.Tap().onStart(() => {
-    birdYVelocity.value = JUMP_FORCE;
-  });
+  const fontFamily = Platform.select({ ios: "Helvetica", default: "serif" });
+  const fontStyle = {
+    fontFamily,
+    fontSize: 40,
+    fontWeight: "bold",
+  };
+  const font = matchFont(fontStyle);
 
   const pipeOffset = 0;
 
@@ -66,7 +108,7 @@ const App = () => {
           <Image width={width} height={height} fit={'cover'} image={bg} />
 
           {/* Pipes */}
-          < Image
+          <Image
             image={pipeTop}
             y={pipeOffset - 320}
             x={x}
@@ -99,11 +141,14 @@ const App = () => {
             <Image
               image={bird}
               y={birdY}
-              x={width / 4}
+              x={birdPosition.x}
               width={64}
               height={48}
             />
           </Group>
+
+          {/* Score */}
+          <Text x={width / 2} y={100} text={score.toString()} font={font} />
         </Canvas>
       </GestureDetector>
     </GestureHandlerRootView>
